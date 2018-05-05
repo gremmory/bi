e8615a7ed15ff47d1072da9a2b128cfdd8983361
Jose Cano <cano19.jose@gmail.com>
2018-05-04 19:57:43 -0600

    nor

--- tests/AcreditarTest.php
+++ tests/AcreditarTest.php
@@ -28,5 +28,7 @@ class AcreditarTest extends TestCase
             ->type('Ingreso Prueba 0', 'desc')
             ->press('btn_realizar')
             ->see('CREDITO EXITOSO: Se ha acreditado correctamente el monto indicado');
+
+            
     }
 }
